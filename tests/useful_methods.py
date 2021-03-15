c8cd56c511415d26d12d02e0827874b79a15e4ce
banteg <banteeg@gmail.com>
2021-03-15 20:15:17 +0300

    feat: add harvest live vault

--- tests/useful_methods.py
+++ tests/useful_methods.py
@@ -1,3 +1,6 @@
+from brownie import Contract, ZERO_ADDRESS
+
+
 def state_of_strategy(strategy, currency, vault):
     scale = 10 ** currency.decimals()
     state = vault.strategies(strategy).dict()
@@ -25,3 +28,15 @@ def state_of_vault(vault, currency):
     print(f"Total Assets: {vault.totalAssets() / scale}")
     print(f"Loose balance in vault: {currency.balanceOf(vault) / scale}")
     print(f"Total Debt: {vault.totalDebt() / scale}")
+
+
+def harvest_live_vault(vault):
+    print(f"Harvesting {vault.name()}")
+    gov = vault.governance()
+    for i in range(20):
+        strat = vault.withdrawalQueue(i)
+        if strat == ZERO_ADDRESS:
+            break
+        strat = Contract(strat)
+        print(f"  Harvesting Strategy {strat.name()}")
+        strat.harvest({'from': gov})
